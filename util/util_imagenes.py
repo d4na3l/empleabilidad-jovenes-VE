35394eed0827f23783a8be04e280b4de3b063b0d
daniel <christian.daniel.ve@gmail.com>
2024-11-22 18:59:39 -0400

    refactor(utils): apply improvements and adjustments to utility files

--- util/util_imagenes.py
+++ util/util_imagenes.py
@@ -1,13 +1,10 @@
-# Importa las clases ImageTk y Image del módulo PIL (Python Imaging Library).
-# ImageTk se utiliza para trabajar con imágenes en aplicaciones Tkinter,
-from PIL import ImageTk, Image
+from PIL import Image, ImageTk
 
-def leer_imagen(path, size):
-    # Abre la imagen desde la ruta especificada utilizando Image.open().
-    # La ruta 'path' debe ser una cadena que indique la ubicación del archivo de imagen.
-    # 'size' es una tupla que especifica el nuevo tamaño de la imagen.
-    # La imagen se redimensiona utilizando el método resize() de la clase Image.
-    
-    # El método resize() toma dos argumentos: el nuevo tamaño y el método de adaptación.
-    # Image.ADAPTIVE se utiliza para ajustar la imagen a las dimensiones especificadas
-    return ImageTk.PhotoImage(Image.open(path).resize(size, Image.ADAPTIVE))
+def leer_imagen(ruta, tamaño):
+    try:
+        imagen = Image.open(ruta)
+        imagen = imagen.resize(tamaño)
+        return ImageTk.PhotoImage(imagen)
+    except Exception as e:
+        print(f"Error al cargar la imagen: {e}")
+        return None
