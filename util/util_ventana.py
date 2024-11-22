dbfa3c3e9ef8919124f40f2e13285b567bb9c6e8
daniel <christian.daniel.ve@gmail.com>
2024-11-22 18:59:39 -0400

    refactor(utils): apply improvements and adjustments to utility files

--- util/util_ventana.py
+++ util/util_ventana.py
@@ -1,16 +1,6 @@
-def centrar_ventana(ventana, aplicacion_ancho, aplicacion_largo):
-    # Obtiene el ancho de la pantalla en píxeles
-    pantall_ancho = ventana.winfo_screenwidth()
-    
-    # Obtiene el alto de la pantalla en píxeles
-    pantall_largo = ventana.winfo_screenheight()
-    
-    # Calcula la posición X para centrar la ventana en la pantalla
-    x = int((pantall_ancho / 2) - (aplicacion_ancho / 2))
-    
-    # Calcula la posición Y para centrar la ventana en la pantalla
-    y = int((pantall_largo / 2) - (aplicacion_largo / 2))
-    
-    # Establece el tamaño de la ventana y su posición en la pantalla
-    # La geometría se define como "ancho x alto + posición_x + posición_y"
-    return ventana.geometry(f"{aplicacion_ancho}x{aplicacion_largo}+{x}+{y}")
+def centrar_ventana(ventana, ancho, alto):
+    pantalla_ancho = ventana.winfo_screenwidth()
+    pantalla_alto = ventana.winfo_screenheight()
+    x = (pantalla_ancho // 2) - (ancho // 2)
+    y = (pantalla_alto // 2) - (alto // 2)
+    ventana.geometry(f"{ancho}x{alto}+{x}+{y}")
